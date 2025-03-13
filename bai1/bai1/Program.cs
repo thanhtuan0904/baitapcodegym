f82a1c509ff1162cd3df9c41ab9e8f440f460ca7
Tuấn <iamtuan090404@gmail.com>
2025-03-13 21:57:40 +0700

    Add bai1 folder content

---
+++ bai1/bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhập a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhập b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        if (a == 0)
+        {
+            if (b == 0)
+                Console.WriteLine("Phương trình có vô số nghiệm.");
+            else
+                Console.WriteLine("Phương trình vô nghiệm.");
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"Nghiệm của phương trình là x = {x}");
+        }
+    }
+}
