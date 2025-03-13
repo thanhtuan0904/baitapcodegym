dfd49a34a10ee7271fca256ead5da6b45bdc78a4
Tuấn <iamtuan090404@gmail.com>
2025-03-13 22:43:39 +0700

    Add bai3 folder

---
+++ bai3/bai3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("nhập năm :");
+        int nam = Convert.ToInt32(Console.ReadLine());
+
+        if (nam % 4 == 0 && nam % 100 != 0 )
+        {
+            Console.WriteLine(" nam nhuan");
+        }
+        else if(nam %400 == 0)
+        {
+            Console.WriteLine("nam nhuan");
+        }
+        else 
+        {
+            Console.WriteLine("nam khong nhuan");
+        }
+    }
+}
