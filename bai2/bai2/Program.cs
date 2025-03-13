89f8eb551a215245bb52ad0c9f2fab43dd8271c5
Tuấn <iamtuan090404@gmail.com>
2025-03-13 22:13:20 +0700

    Add bai2 folder

---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Mời nhập cân nặng :");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Mời nhập chiều cao :");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        double BMI = weight / height * 2;
+
+        if (BMI < 18.5)
+        {
+            Console.WriteLine("Underweight");
+        }
+        else if (BMI >= 18.5 && BMI < 25)
+        {
+            Console.WriteLine("Normal");
+        }
+        else if (BMI >= 25 && BMI < 30)
+        {
+            Console.WriteLine("Overweight");
+        }
+        else if (BMI >= 30)
+        {
+            Console.WriteLine("Obese");
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
