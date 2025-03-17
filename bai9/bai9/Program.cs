6ce7b93890217ce35aae329fb4fe085950e14df0
Tuấn <iamtuan090404@gmail.com>
2025-03-17 18:10:22 +0700

    them bai 9

---
+++ bai9/bai9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ung_dung_menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int luaChon = -1;
+            while (luaChon != 0)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Vẽ tam giác");
+                Console.WriteLine("2. Vẽ hình vuông");
+                Console.WriteLine("3. Vẽ hình chữ nhật");
+                Console.WriteLine("0. Thoát");
+                Console.Write("Nhập lựa chọn của bạn: ");
+                luaChon = Int32.Parse(Console.ReadLine());
+
+                switch (luaChon)
+                {
+                    case 1:
+                        Console.WriteLine("Vẽ tam giác");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("Vẽ hình vuông");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 3:
+                        Console.WriteLine("Vẽ hình chữ nhật");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Lựa chọn không hợp lệ!");
+                        break;
+                }
+            }
+        }
+    }
+}
