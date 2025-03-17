b6beda39684d120af156b37e2aa0c54f26d2e0bd
Tuấn <iamtuan090404@gmail.com>
2025-03-17 18:00:49 +0700

    them bai8

---
+++ bai8/bai8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Các số nguyên tố nhỏ hơn 100:");
+        for (int i = 2; i < 100; i++)
+        {
+            if (soNguyenTo(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+    static bool soNguyenTo(int n)
+    {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) //
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
